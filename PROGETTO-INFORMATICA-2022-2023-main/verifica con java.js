7d934faeee96e982a231c819459c3e102837cfed
Simo <116495710+SimoneVocale@users.noreply.github.com>
2022-12-13 16:22:39 +0100

    Add files via upload

---
+++ PROGETTO-INFORMATICA-2022-2023-main/verifica con java.js
@@ -0,0 +1,12 @@
+
+var us="campo obbligatorio" 
+var pw="campo obbligatorio"
+
+function clicco(){
+   dd document.getElementById("emailerror").innerHTML=us
+   dawd document.getElementById("passworderror").innerHTML=us  
+}
+
+
+
+
